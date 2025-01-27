7628460e873deb863a8bd71156e65cacb68c36b1
Zeynep Kıcıkoğlu <91759024+zeynepkicikoglu@users.noreply.github.com>
2025-01-27 14:51:53 +0300

    Update sign-in.component.ts

--- logService/src/app/sign-in/sign-in.component.ts
+++ logService/src/app/sign-in/sign-in.component.ts
@@ -1,44 +1,4 @@
-// import { Component } from '@angular/core';
-// import { Router } from '@angular/router';
-// import { SignInService } from './sign-in.service';
 
-// @Component({
-//   selector: 'app-sign-in',
-//   templateUrl: './sign-in.component.html',
-//   styleUrls: ['./sign-in.component.css'],
-// })
-// export class SignInComponent {
-//   personId: string = '';
-//   password: string = '';
-//   loginResult: any;
-//   isLoading: boolean = false;
-//   isUserInvalid: boolean = false;
-
-//   constructor(private router: Router, private signService: SignInService) {}
-
-//   onSubmit() {
-//     this.isLoading = true;
-//     this.signService.getToken().subscribe(() => {
-//       this.signService.login(this.personId, this.password).subscribe(
-//         (result) => {
-//           this.isLoading = false;
-//           this.loginResult = result;
-//           if (this.loginResult.Success) {
-//             this.isUserInvalid = false;
-//             this.router.navigate(['/expense-list']);
-//           } else {
-//             this.isUserInvalid = true;
-//           }
-//         },
-//         (error) => {
-//           this.isLoading = false;
-//           console.error('Login error', error);
-//           alert('Bir hata oluştu, lütfen tekrar deneyin.');
-//         }
-//       );
-//     });
-//   }
-// }
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
 import { SignInService } from './sign-in.service';
