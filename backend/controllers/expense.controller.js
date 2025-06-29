5d2e0fd99078a8da0c7fcce40937ffcdc8321493
Zeynep Kıcıkoğlu <91759024+zeynepkicikoglu@users.noreply.github.com>
2025-06-29 12:27:27 +0300

    Commit

--- backend/controllers/expense.controller.js
+++ backend/controllers/expense.controller.js
@@ -36,40 +36,6 @@ export const getExpenseCodes = async (req, res) => {
   }
 };
 
-// export const getCustomers = async (req, res) => {
-//   console.log("getCustomers controlun basındayımmm");
-//   // Authorization headers'ı alın
-//   const authHeader = req.headers.authorization;
-
-//   if (!authHeader) {
-//     return res
-//       .status(400)
-//       .json({ message: "Authorization header is missing." });
-//   }
-
-//   try {
-//     const customers = await ExpenseListService.getCustomers({
-//       Authorization: authHeader,
-//     });
-//     if (customers && customers.length > 0) {
-//       console.log("FRONTENDE GİTTİ", customers);
-//       res.json({
-//         success: true,
-//         customers: customers,
-//       });
-//     } else {
-//       console.log("FRONTENDE GİDEMEDİ, customers bulunamadı");
-//       res.status(404).json({ success: false, message: "customers not found" });
-//     }
-//   } catch (error) {
-//     console.error("customers fetch hatası:", error);
-//     res
-//       .status(500)
-//       .json({ success: false, message: "Error customers expenses" });
-//   } finally {
-//     console.log("Customers request tamamlandı.");
-//   }
-// };
 
 export const getCustomers = async (req, res) => {
   console.log("getCustomers controllunun başındayım");
